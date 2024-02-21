6362d421205a946fd5868d193f3191d54d81fcdb
Kunal sharan <codedragonks07@gmail.com>
2024-02-21 14:27:57 +0530

    update

--- app/Components/addToCart.jsx
+++ app/Components/addToCart.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import cart from "../../public/cart.jpg";
+import bamboo from "../../public/bamboo.jpg";
 
 const AddToCartPage = () => {
   const [quantity, setQuantity] = useState(1);
@@ -23,22 +23,33 @@ const AddToCartPage = () => {
   return (
     <div className="flex p-12 bg-white-green h-screen">
       <div className="w-1/2 pr-8">
-        <Image src={cart} alt="Product" className="rounded-md w-full h-full" />
+        <Image
+          src={bamboo}
+          alt="Product"
+          width={200}
+          height={200}
+          className="rounded-md w-full h-full"
+        />
       </div>
       <div className="w-1/2">
-        <h2 className="text-6xl text-black font-bold font-dancing mb-4">
-          Sweet Serenity Bliss Candle
+        <h2 className="text-5xl text-black font-bold font-dancing mb-4">
+          TriadCascade Bamboo Fountain | Without Basebowl
         </h2>
-        <p className="text-dark-green font-normal mb-4 text-3xl font-crimson">
-          Experience the perfect blend of sweetness and serenity with our Sweet
-          Serenity Bliss Candle. Immerse yourself in the delightful notes of
-          vanilla and chamomile, creating an atmosphere of pure tranquility.
-          Hand-poured with care using eco-friendly soy wax, this candle promises
-          a clean, prolonged burn. Elevate your space with the harmonious fusion
-          of sweetness and serenity.
+        <p className="text-dark-green font-normal mb-4 text-md font-crimson">
+          Experience the serenity of our Bamboo Water Fountain, meticulously
+          handcrafted from sustainably sourced bamboo. Its timeless design,
+          inspired by Zen aesthetics, seamlessly integrates into any space,
+          serving as both a striking decor piece and a meditation tool. Beyond
+          its aesthetic appeal, this fountain offers a myriad of health
+          benefits, acting as an air purifier by releasing negative ions to
+          cleanse the atmosphere and as a humidity balancer, providing moisture
+          to dry environments, beneficial for skin and overall well-being. Let
+          the gentle trickle of water lull you into relaxation, promoting better
+          sleep and enhancing your daily living experience with its natural
+          beauty and functionality.
           <br />
           <br />
-          <strong className="text-sec-red text-3xl font-crimson">
+          <strong className="text-sec-red text-xl font-crimson">
             Available For Just Rs.800
           </strong>
         </p>
@@ -97,7 +108,7 @@ const AddToCartPage = () => {
         </div>
         <button
           onClick={handleClick}
-          className="bg-sec-red text-white p-2 rounded-lg w-1/6 font-crimson text-2xl"
+          className="bg-sec-red text-white p-2 rounded-lg w-1/6 font-crimson text-xl"
         >
           Add to Cart
         </button>
